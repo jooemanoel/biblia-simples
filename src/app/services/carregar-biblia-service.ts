c6a0c9e3992080b07fdac547629b7646afaf126b
Joao Emanoel Rodrigues Mota <jooemanoel@gmail.com>
2026-03-11 23:52:05 -0300

    Vai tudo junto

--- src/app/services/carregar-biblia-service.ts
+++ src/app/services/carregar-biblia-service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import ARA from "../../assets/ARA.json";
+import NVI from "../../assets/NVI.json";
+import NVT from "../../assets/NVT.json";
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +12,9 @@ export class CarregarBibliaService {
   constructor(private http: HttpClient) {}
 
   loadVersion(version: string): Observable<any> {
+    if (version === 'ARA') return of(ARA);
+    if (version === 'NVI') return of(NVI);
+    if (version === 'NVT') return of(NVT);
     return this.http.get(`/assets/${version}.json`);
   }
 }
